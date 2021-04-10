b819051323704d5059ff073bdd1db3bb3d454eba
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-04-10 18:26:57 +0530

    first commit with product data model

---
+++ android/app/src/main/kotlin/co/veerappa/max_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.veerappa.max_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shop'),
+      ),
+      body: Center(
+        child: Text('Lets build!'),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
