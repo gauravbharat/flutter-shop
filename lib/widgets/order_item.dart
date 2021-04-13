97f5481f3b40abc121873dcf965c990d55f39bb1
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-04-13 14:50:49 +0530

    show order detail

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:math';
 import 'package:max_shop/providers/orders.dart' show OrderItem;
 
-class OrderItemWidget extends StatelessWidget {
+class OrderItemWidget extends StatefulWidget {
   final OrderItem order;
   OrderItemWidget(this.order);
+
+  @override
+  _OrderItemWidgetState createState() => _OrderItemWidgetState();
+}
+
+class _OrderItemWidgetState extends State<OrderItemWidget> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +21,48 @@ class OrderItemWidget extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime)),
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              height: min(widget.order.products.length * 20.0 + 10.0, 180),
+              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 4.0),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${prod.quantity} x \$${prod.price}',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
