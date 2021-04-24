da785a046abfbe2382c3e25144d7c42675272805
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-04-24 19:37:26 +0530

    add logout button

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -73,4 +73,11 @@ class Auth with ChangeNotifier {
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logout() {
+    _token = null;
+    _expiryDate = null;
+    _userId = null;
+    notifyListeners();
+  }
 }
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:max_shop/pages/auth_screen.dart';
 import 'package:max_shop/pages/orders_page.dart';
 import 'package:max_shop/pages/products_overview_page.dart';
 import 'package:max_shop/pages/user_products_page.dart';
+import 'package:max_shop/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -36,6 +39,15 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(UserProductsPage.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              // Navigator.of(context).pop();
+              Provider.of<Auth>(context, listen: false).logout();
             },
           ),
         ],
